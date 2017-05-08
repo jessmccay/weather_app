f6b6653b41b904bc9949e44683df48cd926968bf
jessmccay <jessmccay@gmail.com>
2017-05-08 11:24:13 -0700

    toggle, css, F or C function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var x = document.getElementById("demo");
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -6,7 +7,20 @@ function getLocation() {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
+
+$(document).ready(function () {
+  $(".slider").on("click", function () {
+   if($("#temp").value() == "C") {
+    $("#temp").html = "F";
+   }
+   else{
+       $("#temp").html = "C";
+    }
+});
+
+   });
