234ec5ccb1b04b2f3b25e6d6f4d8c41852e60a93
jessmccay <jessmccay@gmail.com>
2017-05-12 12:14:04 -0700

    weathermap.org api and ip-api api

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
-// example api //ajax call
-// if (navigator.geolocation) {
-//     //Return the user's longitude and latitude on page load using HTML5 geolocation API
-//     window.onload = function () {
-//     var currentPosition;
-//     function getCurrentLocation (position) {
-//         currentPosition = position;
-//         latitude = currentPosition.coords.latitude;
-//         longitude = currentPosition.coords.longitude;
-//         //AJAX request
-//         $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=******************", function (data) {
-//             var rawJson = JSON.stringify(data);
-//             var json = JSON.parse(rawJson);
-//             updateWeather(json); //Update Weather parameters
-//         });
-//     }
-//     navigator.geolocation.getCurrentPosition(getCurrentLocation);
-//     };
-var x = document.getElementById("demo");
+$(document).ready(function (){
+  var long;
+  var lat;
+  $.getJSON("http://ip-api.com/json", function(data2){
+    lat=data2.lat;
+    long=data2.lon;
+    console.log(lat);
+    var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=dd82da92994f7e7e7c175d467aa1bb78';
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+  $.getJSON(api, function(data){
+   // alert(data.coord.lat);
+    // var far;
+    // var cel;
+    var typeOfWeather = data.weather[0].description;
+    kel = data.main.temp;
+    var wind = data.wind.speed;
+    var city = data.name;
+  far = (kel)*(9/5)-459.67;
+  cel= kel-273;
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
+     console.log(city);
+    $('#city').html(city);
+    $('#typeOfWeather').html(typeOfWeather)
+$('#far').html(far);
 
-$(document).ready(function () {
-  $(".slider").on("click", function () {
-   if($("#temp").value() == "C") {
-    $("#temp").html = "F";
-   }
-   else{
-       $("#temp").html = "C";
-    }
+  })
+
+  // $('#data').html("latitude: " + lat + "<br>longitude: " + long);
+
+  });
 });
-
-   });
