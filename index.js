ea80a949eb86fade798f89eca94c0a26d22301e5
jessmccay <jessmccay@gmail.com>
2017-05-11 21:05:49 -0700

    sample api.. ajax

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// example api //ajax call
+// if (navigator.geolocation) {
+//     //Return the user's longitude and latitude on page load using HTML5 geolocation API
+//     window.onload = function () {
+//     var currentPosition;
+//     function getCurrentLocation (position) {
+//         currentPosition = position;
+//         latitude = currentPosition.coords.latitude;
+//         longitude = currentPosition.coords.longitude;
+//         //AJAX request
+//         $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=******************", function (data) {
+//             var rawJson = JSON.stringify(data);
+//             var json = JSON.parse(rawJson);
+//             updateWeather(json); //Update Weather parameters
+//         });
+//     }
+//     navigator.geolocation.getCurrentPosition(getCurrentLocation);
+//     };
 var x = document.getElementById("demo");
 
 function getLocation() {
